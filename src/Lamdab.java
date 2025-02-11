82f53f54650feae003a8a19212bb384d039e237e
malozano49 <128610674+malozano49@users.noreply.github.com>
2025-02-11 11:14:30 +0100

    Update Lamdab.java

--- src/Lamdab.java
+++ src/Lamdab.java
@@ -8,7 +8,7 @@ public class Lamdab {
 	
 	
 	
-	
+	//escrito en git
 	public static void main(String[] args) {
 		String[] vowels = {"a","e","i","o","u"};
 		List<String> vowelsList = Arrays.asList(vowels);
@@ -22,13 +22,13 @@ public class Lamdab {
 		/**.filter(Obj -> condicion): filtro*/
 		List<Integer> lista = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 		/**
-		x: es el nombre del objeto de la expresi髇 lamdab filter() 
-		.filter(condici髇 [x==5 o objeto]): filtrar los elementos de la lista con una condicioen
+		x: es el nombre del objeto de la expresi贸n lamdab filter() 
+		.filter(condici贸n [x==5 o objeto]): filtrar los elementos de la lista con una condicioen
 			ejemplos:
 			 	.filter(x -> x==5)
 			 	.filter(c -> c.getPoints() > 100)
 		.forEach(): recorrer la lista como un for
-			n: es el nombre del objeto dentro de la expresi髇 lamdab .forEach() 
+			n: es el nombre del objeto dentro de la expresi贸n lamdab .forEach() 
 			.forEach((n) -> System.out.println("prueba: "+ n)
 		*/
 		/*
@@ -43,8 +43,8 @@ public class Lamdab {
 		
 		/**
 		 * 
-		 * El uso de Java Stream map es una de las operaciones m醩 comunes cuando trabajamos con un flujo de Streams .
-		 * El m閠odo map nos permite realizar una transformaci髇 r醦ida de los datos y muy directa sobre el flujo original
+		 * El uso de Java Stream map es una de las operaciones m谩s comunes cuando trabajamos con un flujo de Streams .
+		 * El m茅todo map nos permite realizar una transformaci贸n r谩pida de los datos y muy directa sobre el flujo original
 		 * 
 		 * 
 		 * 
@@ -59,9 +59,9 @@ public class Lamdab {
 		        .map(Integer::valueOf)
 		        .collect(Collectors.toList());
 		/**
-		 *  collect () se usa principalmente para recopilar los elementos de flujo en una colecci髇.List<Integer> listOfIntegers
+		 *  collect () se usa principalmente para recopilar los elementos de flujo en una colecci贸n.List<Integer> listOfIntegers
 		 * .collect(Collectors.toList()
-		 * colecci髇= List<Integer> listOfIntegers
+		 * colecci贸n= List<Integer> listOfIntegers
 		 * */ 
 		//System.out.println(listOfIntegers);
 		/**
@@ -73,7 +73,7 @@ public class Lamdab {
 		/ lista.stream().sorted(Comparator.comparing(ObjetoPersona::Getter del un atributo)).forEach(System.out::println);
 		 *		ejemplo: lista.stream().sorted(Comparator.comparing(Persona::getApellidos)).forEach(System.out::println);
 		 * 		ejemplo de ODA: .sorted(Comparator.comparing(OpacJobInstance::getId).reversed())
-		 * .reversed(): metodo que invierte el orden de clasificaci髇 del original.
+		 * .reversed(): metodo que invierte el orden de clasificaci贸n del original.
 		 */
 		
 		/**
