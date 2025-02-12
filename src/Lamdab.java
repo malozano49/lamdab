23549dff695bcb7f80a14529313ed2f5acfc1fd4
miguel_angel.de_dieg <miguel_angel.de_dieg@baratz.es>
2025-02-12 12:18:56 +0100

    Main 12:18

--- src/Lamdab.java
+++ src/Lamdab.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lamdab {
-
+//Main h 12:18
 	public static void main(String[] args) {
 		String[] vowels = {"a","e","i","o","u"};
 		List<String> vowelsList = Arrays.asList(vowels);
